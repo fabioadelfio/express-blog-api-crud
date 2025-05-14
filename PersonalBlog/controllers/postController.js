5598efe157e98495e7580b87e80529ea79ce2282
fabioadelfio <adelfiofabio07@gmail.com>
2025-05-14 14:08:07 +0200

    destroy

--- PersonalBlog/controllers/postController.js
+++ PersonalBlog/controllers/postController.js
@@ -16,7 +16,7 @@ const index = (req, res) => {
 const show = (req, res) => {
     const id = parseInt(req.params.id);
     const post = posts.find(p => p.id === id);
-    post ? res.json(post) : res.status(404).send(`Post con id ${id} non trovato`);
+    post ? res.json(post) : res.status(404).send({ error: `Post con id ${id} non trovato` });
 };
 
 const create = (req, res) => {
@@ -30,7 +30,22 @@ const update = (req, res) => {
 
 const destroy = (req, res) => {
     const id = parseInt(req.params.id);
-    res.send(`Cancellazione del post ${id}`);
+
+    const post = posts.find(p => p.id === id);
+
+    if(!post) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: `Not Found`,
+            message: `Post non trovato`
+        })
+    }
+
+    posts.splice(posts.indexOf(id), 1);
+
+    res.sendStatus(204);
 };
 
 module.exports = { index, show, create, update, destroy };
